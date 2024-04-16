227215c626bca01875cf56567e3b9b3c3cfcf34d
kamilsalakhov <salakhovkamilbr@gmail.com>
2024-04-17 01:45:53 +0300

    fixed bag in hello message

--- resources/messages/messages.py
+++ resources/messages/messages.py
@@ -1,7 +1,7 @@
 HELLO_MESSAGE = r"""
 🌟✨ Hey there! Welcome to our Telegram bot! ✨🌟
 
-I'm here to assist you in generating posts using our templates_util. Simply select the template you need, fill in the required fields, and your post will be ready for publishing. If you have any questions or suggestions, feel free to reach out.
+I'm here to assist you in generating posts. Simply select the template you need, fill in the required fields, and your post will be ready for publishing. If you have any questions or suggestions, feel free to reach out.
 
 Happy posting! 😊🚀
 """
